8573cdad7aaad2ec9bf11a78e4969cb818cc5218
JoelMorel <Joel@JoelMorel.com>
2020-10-30 17:54:08 -0400

    updated getData() to properly return data to chart

--- js/main.js
+++ js/main.js
@@ -5,21 +5,19 @@ Chart.defaults.global.defaultFontFamily = "Lato";
 Chart.defaults.global.animation.onComplete = () => {
   Chart.defaults;
 };
-const xLabels = [];
-const payout = [];
 
 createChart();
 
 async function createChart() {
-  await getData();
+  const data = await getData();
   let myChart = new Chart(ctx, {
     type: "bar",
     data: {
-      labels: xLabels,
+      labels: data.xLabels,
       datasets: [
         {
           label: "2018 Payout %",
-          data: payout,
+          data: data.yLabels,
           borderWidth: 1,
         },
       ],
@@ -32,7 +30,7 @@ async function createChart() {
         yAxes: [
           {
             ticks: {
-              beginAtZero: true,
+              beginAtZero: false,
             },
           },
         ],
@@ -42,9 +40,11 @@ async function createChart() {
 }
 
 async function getData() {
-  const response = await fetch("AC Slot Machine Data 2018 - Sheet1.csv");
+  const xLabels = [];
+  const yLabels = [];
+
+  const response = await fetch("/AC Slot Machine Data 2018 - Sheet1.csv");
   const data = await response.text();
-  //   const column = [];
 
   const table = data.split("\n").slice(1);
   table.forEach((row) => {
@@ -53,8 +53,9 @@ async function getData() {
     const payoutPercentage = column[1];
 
     xLabels.push(casino);
-    payout.push(parseFloat(payoutPercentage));
+    yLabels.push(100 - parseFloat(payoutPercentage));
   });
+  return { xLabels, yLabels };
 }
 
 // const payoutSlotMachine1Cents = column[1];
